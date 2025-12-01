c36ae1e2cee2d9b0d18963f29ac9f9eb3898ef53
Leonardo Moreira <lgmo@cin.ufpe.br>
2025-12-01 18:39:27 -0300

    add vim-illuminate

--- lua/lgmo/appearence.lua
+++ lua/lgmo/appearence.lua
@@ -1,4 +1,5 @@
-vim.cmd.colorscheme('miasma')
+vim.cmd.colorscheme('rose-pine')
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 --vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 --vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
 --vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
--- lua/lgmo/mappings.lua
+++ lua/lgmo/mappings.lua
@@ -304,4 +304,4 @@ vim.keymap.set("v", "<", "<gv", { desc = "Unindent preserving selection" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
+vim.keymap.set("n", "<space>fb", ":Dirs<CR>")
